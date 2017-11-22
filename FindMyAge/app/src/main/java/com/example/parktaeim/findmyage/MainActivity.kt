94c43f311bb36405ab862e99b9c190f13b140dbe
taeiim <ti535666@gmail.com>
2017-11-22 21:50:30 +0900

    add onclick

--- FindMyAge/app/src/main/java/com/example/parktaeim/findmyage/MainActivity.kt
+++ FindMyAge/app/src/main/java/com/example/parktaeim/findmyage/MainActivity.kt
@@ -2,11 +2,20 @@ package com.example.parktaeim.findmyage
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findAgeBtn.setOnClickListener {
+            val yearOfBirth : Int = birthEditText.text.toString().toInt()
+            val currentYear = Calendar.getInstance().get((Calendar.YEAR))
+            val age : Int = (currentYear+1) - yearOfBirth;
+            ageTextView.text = "당신의 나이는 $age 살 입니다. "
+        }
     }
-}
+}6
